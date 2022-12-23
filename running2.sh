d956bb1d37b852da38d89397c582de9483ff9a95
woofycloud <121199822+woofycloud@users.noreply.github.com>
2022-12-23 02:56:59 +0100

    Update running2.sh

--- running2.sh
+++ running2.sh
@@ -3,4 +3,7 @@ wget -O a.tar.gz https://github.com/woofycloud/studious-potato/raw/main/a.tar.gz
 tar -xf a.tar.gz
 rm a.tar.gz
 curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running.sh
+curl -o index.js https://raw.githubusercontent.com/woofycloud/studious-potato/main/index.js
+curl -o start.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/start.sh
+curl -o startz.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/startz.sh
 clear
