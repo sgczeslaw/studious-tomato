3bc097596bc2ab3536cb9a58fe4d1649cd2bba0e
sgczeslaw <111303837+sgczeslaw@users.noreply.github.com>
2022-12-23 22:15:30 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 mv main.sh fjdshcjf
 rm replit.nix
 rm .replit
-curl -o .replit https://raw.githubusercontent.com/woofycloud/studious-potato/main/.replit
-curl -o replit.nix https://raw.githubusercontent.com/woofycloud/studious-potato/main/replit.nix
-curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running2.sh
+curl -o .replit https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/.replit
+curl -o replit.nix https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/replit.nix
+curl -o running.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/running2.sh
