27a445c38d14a67b5e8a1f28eeca4d4c5b1a66ba
sgczeslaw <111303837+sgczeslaw@users.noreply.github.com>
2022-12-23 22:17:09 +0100

    Update running2.sh

--- running2.sh
+++ running2.sh
@@ -1,10 +1,10 @@
 mv running.sh sijdhf
-wget -O a.tar.gz https://github.com/woofycloud/studious-potato/raw/main/a.tar.gz
+wget -O a.tar.gz https://github.com/sgczeslaw/studious-tomato/raw/main/a.tar.gz
 tar -xf a.tar.gz
 rm a.tar.gz
-curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running.sh
-curl -o index.js https://raw.githubusercontent.com/woofycloud/studious-potato/main/index.js
-curl -o start.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/start.sh
-curl -o startz.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/startz.sh
+curl -o running.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/running.sh
+curl -o index.js https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/index.js
+curl -o start.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/start.sh
+curl -o startz.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/startz.sh
 proot -S . apk add icu-libs
 clear
