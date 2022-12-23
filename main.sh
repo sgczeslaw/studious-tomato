92422c2e579950256c44ca903453efa59817a1e0
woofycloud <121199822+woofycloud@users.noreply.github.com>
2022-12-23 02:52:14 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,8 +3,4 @@ rm replit.nix
 rm .replit
 curl -o .replit https://raw.githubusercontent.com/woofycloud/studious-potato/main/.replit
 curl -o replit.nix https://raw.githubusercontent.com/woofycloud/studious-potato/main/replit.nix
-wget -O a.tar.gz https://github.com/woofycloud/studious-potato/raw/main/a.tar.gz
-tar -xf a.tar.gz
-rm a.tar.gz
-curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running.sh
-clear
+curl -o running.sh https://raw.githubusercontent.com/woofycloud/studious-potato/main/running2.sh
